095616bbb5d9262aff5bf9fca99f49967eef7505
sishirsubedi <sishirsubedi@tamu.edu>
2017-12-30 00:06:40 -0600

    "updated rf"

--- 1_ind_modeling.py
+++ 1_ind_modeling.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score, learning_curve, KFold
 from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 
 
 df_all_data = pd.read_csv("../df_master_all.csv",sep=',',header=0)
@@ -50,3 +54,59 @@ for train, test in kf.split(df_data):
 
 print np.sum(result)/len(result)
 
+
+
+
+
+################## random forest
+'''
+There are 4 hyperparameters required for a Random Forest classifier;
+
+    the number of trees in the forest (n_estimators).
+    the number of features to consider at each split. By default: square root of total number of features (max_features).
+    the maximum depth of a tree i.e. number of nodes (max_depth).
+    the minimum number of samples required to be at a leaf node / bottom of a tree (min_samples_leaf).
+
+'''
+
+
+xtrain = df_all_data.loc[:,'87_mean':'insurance_Self Pay']
+ytrain = pd.DataFrame(df_all_data.loc[:,'IsReadmitted'])
+
+seed =123
+
+# RFC with fixed hyperparameters max_depth, max_features and min_samples_leaf
+rfc = RandomForestClassifier(n_jobs=-1, oob_score=True, max_depth=10, max_features='sqrt', min_samples_leaf=10)
+
+# Range of `n_estimators` values to explore.
+n_estim = filter(lambda x: x % 2 == 0, list(range(10, 20)))
+
+cv_scores = []
+
+for i in n_estim:
+    rfc.set_params(n_estimators=i)
+    kfold = KFold(n_splits=3, random_state=seed)
+    scores = cross_val_score(rfc, xtrain, ytrain.values, cv=kfold, scoring='accuracy')
+    cv_scores.append(scores.mean() * 100)
+
+optimal_n_estim = n_estim[cv_scores.index(max(cv_scores))]
+print "The optimal number of estimators is %d with %0.1f%%" % (optimal_n_estim, cv_scores[optimal_n_estim])
+
+plt.plot(n_estim, cv_scores)
+plt.xlabel('Number of Estimators')
+plt.ylabel('Train Accuracy')
+plt.show()
+
+############## optimize all three parameters
+
+rfc = RandomForestClassifier(n_jobs=-1, max_features='sqrt', oob_score=True)
+
+# Use a grid over parameters of interest
+param_grid = {
+    "n_estimators": [9, 18, 27, 36, 45, 54, 63],
+    "max_depth": [1, 5, 10, 15, 20, 25, 30],
+    "min_samples_leaf": [1, 2, 4, 6, 8, 10]}
+
+CV_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=3)
+CV_rfc.fit(xtrain, df_all_data.IsReadmitted)
+print CV_rfc.best_params_
